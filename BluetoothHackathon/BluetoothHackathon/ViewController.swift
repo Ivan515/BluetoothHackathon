0ad4436ab4765d67224a13fdc26c71e5ce9c1154
Ivan Apet <van.ka515@gmail.com>
2018-02-24 16:14:21 +0300

    Initial commit

---
+++ BluetoothHackathon/BluetoothHackathon/ViewController.swift
@@ -0,0 +1,21 @@
+//Copyright 2018 Idealnaya rabota LLC
+//Licensed under Multy.io license.
+//See LICENSE for details
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
